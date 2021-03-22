1607ca700831070e97d61f5015161339c28010ec
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-22 14:48:49 +0100

    1

--- Algoritmi.js
+++ Algoritmi.js
@@ -25,6 +25,65 @@ const reverse = (str) => {
     return revString;
 }
 
+//palindrom
+
+const isPalindrom = (str) => {
+    const revString = str.split('').reverse().join('');
+    if (revString === str){
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+const reverseInt = (int) => {
+    const revString = int.toString().split('').reverse().join('');
+
+    return parseInt(revString);
+}
+
+const capitilize = (str) =>{
+    const strArr = str.toLowerCase().split(' ');
+
+    // substring prvo slovo
+    // split(' ') -> sa spaceom split by words
+
+    for(let i = 0; i < strArr.length; i++){
+        strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+    }
+
+    return strArr.join(' ');
+}
+
+// const maxChar = (str) =>{
+
+// }
+
+const fizzBuzz = () => {
+    for(let i = 1; i <=100; i++) {
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log('FizzBuzz');
+        }        
+        else if(i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0){
+            console.log('Buzz');
+        } 
+        else {
+            console.log(i);
+        }
+    }
+}
+
 const output = reverse('hello');
+const palin = isPalindrom('hello');
+const intRev = reverseInt('12345');
+const cap = capitilize('i love js');
+const runFizz = fizzBuzz();
 
 console.log(output);
+console.log(palin);
+console.log(intRev);
+console.log(cap);
+console.log(runFizz);
